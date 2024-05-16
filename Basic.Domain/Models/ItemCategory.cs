295772506e270703500f653f26ec1bb7ea0b0c90
Adeeb Bin Islam <adeeb.islam@wundermanthompson.com>
2024-05-16 17:43:53 +0600

    All changes done (JWT Authentication, Role Apply)

---
+++ Basic.Domain/Models/ItemCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Domain.Models
+{
+    public class ItemCategory
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public int ItemUnit { get; set; }
+        public int ItemQuantity { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
